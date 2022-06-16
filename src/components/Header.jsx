6e4bb64b6f9bf04e76f0a53097224b2644a4c7ab
Bipin Rai <raibpn>
2022-06-16 19:52:07 +0200

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Button from "./Button";
+
+const Header = ({ title }) => {
+  return (
+    <header className="header">
+      <h1>{title}</h1>
+      <Button />
+    </header>
+  );
+};
+
+export default Header;
