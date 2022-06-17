28afceb3999296c86a131bb1d6437e81adc35cf0
Bipin Rai <raibpn>
2022-06-17 13:35:07 +0200

    task delete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,53 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import { useState } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctors Appointment",
+      day: "Feb 5th at 2:30 pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Meet Numa Rani",
+      day: "Feb 14th at 10 am",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Go to Meeting",
+      day: "Feb 5th at 9:30 am",
+      reminder: true,
+    },
+    {
+      id: 4,
+      text: "Go to Movie",
+      day: "Feb 10th at 2:30 pm",
+      reminder: true,
+    },
+    {
+      id: 5,
+      text: "Go to Walk",
+      day: "Feb 10th at 6:30 pm",
+      reminder: false,
+    },
+  ]);
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((tasks) => tasks.id !== id));
+  };
+
   return (
     <div className="container">
       <Header title={"Task Tracker"} />
-      <Tasks />
+      {tasks?.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} />
+      ) : (
+        "No Tasks"
+      )}
     </div>
   );
 }
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,41 +1,21 @@
 import React from "react";
-const tasks = [
-  {
-    id: 1,
-    text: "Doctors Appointment",
-    day: "Feb 5th at 2:30 pm",
-    reminder: true,
-  },
-  {
-    id: 2,
-    text: "Meet Numa Rani",
-    day: "Feb 14th at 10 am",
-    reminder: true,
-  },
-  {
-    id: 3,
-    text: "Go to Meeting",
-    day: "Feb 5th at 9:30 am",
-    reminder: true,
-  },
-  {
-    id: 4,
-    text: "Go to Movie",
-    day: "Feb 10th at 2:30 pm",
-    reminder: true,
-  },
-  {
-    id: 5,
-    text: "Go to Walk",
-    day: "Feb 10th at 6:30 pm",
-    reminder: false,
-  },
-];
-const Tasks = () => {
+// import Task from "./Task";
+import { FaTimes } from "react-icons/fa";
+
+const Tasks = ({ tasks, onDelete }) => {
   return (
     <>
       {tasks.map((tasks) => (
-        <h3 key={tasks.id}>{tasks.text}</h3>
+        <div className="task" key={tasks.id}>
+          <h3>
+            {tasks.text}
+            <FaTimes
+              style={{ color: "red", cursor: "pointer" }}
+              onClick={() => onDelete(tasks.id)}
+            />
+          </h3>
+          <p>{tasks.day}</p>
+        </div>
       ))}
     </>
   );
