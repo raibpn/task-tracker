17e2e913de3c19d97806b71f6a139ca594ce123f
Bipin Rai <raibpn>
2022-06-17 17:14:17 +0200

    add and delete using fetch

--- src/App.js
+++ src/App.js
@@ -22,13 +22,26 @@ function App() {
   const [showAddTask, setShowAddTask] = useState(false);
   const [btnColor, setBtnColor] = useState("");
 
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 10000) + 1;
-    const newTask = { id, ...task };
-    setTasks([...tasks, newTask]);
+  const addTask = async (task) => {
+    // const id = Math.floor(Math.random() * 10000) + 1;
+    // const newTask = { id, ...task };
+    // setTasks([...tasks, newTask]);
+    const res = await fetch("http://localhost:5000/tasks", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(task),
+    });
+
+    const data = await res.json();
+    setTasks([...tasks, data]);
   };
 
-  const deleteTask = (id) => {
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: "DELETE",
+    });
     setTasks(tasks.filter((tasks) => tasks.id !== id));
   };
 
